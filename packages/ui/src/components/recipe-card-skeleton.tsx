457e51290ca212028b3fdbc9bff11e26aea89a85
Joona Väänänen <joona.vaananen@crasman.fi>
2023-11-26 20:55:20 +0200

    fix recipe card skeleton to match actual recipe card styles

--- packages/ui/src/components/recipe-card-skeleton.tsx
+++ packages/ui/src/components/recipe-card-skeleton.tsx
@@ -1,19 +1,25 @@
-import { Card, Inset } from '@radix-ui/themes';
+import { AspectRatio, Card, Grid, Inset } from '@radix-ui/themes';
+
 import { Skeleton } from './skeleton';
 
 type RecipeCardSkeletonProps = React.ComponentPropsWithoutRef<typeof Card>;
 
 export const RecipeCardSkeleton = (props: RecipeCardSkeletonProps) => {
   return (
-    <Card {...props}>
+    <Card className={'h-full'} {...props}>
       <Inset clip={'padding-box'} side={'top'} pb={'current'}>
-        <div className={'relative pb-[calc(100%/(3/2))]'}>
+        <AspectRatio ratio={3 / 2}>
           <Skeleton
             className={'absolute left-0 top-0 h-full w-full rounded-b-[0px]'}
           />
-        </div>
+        </AspectRatio>
       </Inset>
-      <Skeleton className={'h-7 w-1/2'} />
+      <Grid columns={'2'} gap={'2'}>
+        <Skeleton className={'h-5 w-1/2'} />
+        <Skeleton className={'col-span-full h-[30px] w-1/2'} />
+        <Skeleton className={'mr-auto h-6 w-20'} />
+        <Skeleton className={'ml-auto h-6 w-16'} />
+      </Grid>
     </Card>
   );
 };
