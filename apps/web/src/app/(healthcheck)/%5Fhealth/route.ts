86fdca77546384e673823a49d91c58a7735b17b3
Joona Väänänen <joona.vaananen@crasman.fi>
2023-12-05 17:44:41 +0200

    set hostname and port envs in dockerfile for web app

--- apps/web/src/app/(healthcheck)/%5Fhealth/route.ts
+++ apps/web/src/app/(healthcheck)/%5Fhealth/route.ts
@@ -1,6 +1,3 @@
-// TODO: Debug the following error:
-// Failure in name='port listening check': container listening on expected port (3000) with unexpected IPv4 interface: expected=0.0.0.0 actual=172.17.0.4
-
 export const GET = () => {
   return new Response(null, { status: 204 });
 };
